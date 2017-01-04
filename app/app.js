798ffbcea7216f10d3dfd7cac7e07563174b3d9e
Joe Boyle <joe.boyle@jibo.com>
2017-01-03 22:53:48 -0500

    Moving logic to Game class

--- app/app.js
+++ app/app.js
@@ -1,13 +1,5 @@
 const game = new Game();
 
-const STAGGER_TIME = 30;
-const SQUARE_WIDTH = 380;
-const BOARD_SCALE_PCT = 0.8;
-
-let xOffset;
-let yOffset;
-let bg;
-
 const renderer = new PIXI.autoDetectRenderer(1280, 720, {
     view: document.getElementById("stage"),
     backgroundColor: 0x0,
@@ -22,97 +14,27 @@ function setTheTable(test) {
     bg = test.bg;
     bg.gotoAndStop(0);
     stage.addChild(board);
-    
+
     // This is gnarly. Is there a better way to stagger animation of squares appearing?
-    for (let i = 0; i < game.board.length; i++){
+    for (let i = 0; i < game.boardArr.length; i++){
         (function (idx) {
             setTimeout(()=>{
-                for (let j = 0; j < game.board[i].length; j++) {
+                for (let j = 0; j < game.boardArr[i].length; j++) {
                     (function (idx) {
                         setTimeout(()=>{
-                            addSquare(j, i);
+                            game.addSquare(j, i);
                         }, STAGGER_TIME * idx);
                     }(j));
                 }
-            }, STAGGER_TIME * (idx + (game.board.length * idx)));
+            }, STAGGER_TIME * (idx + (game.boardArr.length * idx)));
         }(i));
     }
-        
+
     board.x = stage.width / 2;
     board.y = stage.height / 2 - 50;
-    board.scale.x = board.scale.y = (stage.width / (SQUARE_WIDTH * game.board.length)) * BOARD_SCALE_PCT;
-    
-    console.log(game.board);
-}
+    board.scale.x = board.scale.y = (stage.width / (SQUARE_WIDTH * game.boardArr.length)) * BOARD_SCALE_PCT;
 
-function addSquare(x, y) {
-    // TODO: Combine test.js, square.js, checker.js, reference library? How?
-    PIXI.animate.load(lib.square, board, (square) => {
-
-        xOffset = square.width / 2;
-        yOffset = square.height / 2;
-        square.x = ( x - y ) * xOffset;
-        square.y = ( y + x ) * yOffset;
-        square.hitArea = new PIXI.Polygon([-xOffset, 0, 0, yOffset, xOffset, 0, 0, -yOffset]);
-        
-        let state = square.state;
-        let frame = 0;
-        state.gotoAndStop(frame);
-        
-        square.directions.gotoAndStop(game.board[x][y]);
-        
-        if (( x + y ) % 2 === 0) {
-            square.color02.visible = false;
-        } else {
-            square.color01.visible = false;
-        }
-        
-        square.interactive = true;
-
-        square.on('click', ()=>{
-            // TODO: remove click listener from other squares
-            dropChecker(x, y);
-        });
-        
-        PIXI.animate.Animator.play(square, 'fadeIn');
-    }, 'assets');
-}
-
-function dropChecker(x, y) {
-    PIXI.animate.load(lib.checker, board, (checker)=>{
-    // let checker = lib.checkerAnim;
-    // console.log(checker);
-        placeChecker(checker, x, y);
-        
-        PIXI.animate.Animator.play(checker, 'dropIn', ()=>{
-            moveChecker(checker, x, y);
-        });
-    }, 'assets');
-}
-
-function moveChecker(instance, x, y) {
-    PIXI.animate.Animator.play(instance, 'move' + game.board[x][y], ()=>{
-        if (!game.spot.x) {
-            game.spot.x = x;
-            game.spot.y = y;
-        }
-        game.walk(game.board, x, y);
-        if (game.state.length) {
-            bg.gotoAndStop(game.state);
-            if (game.state === 'off') {
-                return;
-            }
-        }
-        console.log(game.spot);
-        instance.gotoAndStop('pause');
-        placeChecker(instance, game.spot.x, game.spot.y);
-        moveChecker(instance, game.spot.x, game.spot.y);
-    });
-}
-
-function placeChecker(instance, x, y) {
-    instance.x = ( x - y ) * xOffset;
-    instance.y = ( y + x ) * yOffset;
+    console.log(game.boardArr);
 }
 
 function update() {
--- app/game.js
+++ app/game.js
@@ -3,111 +3,171 @@ const states = {
     LOOP: 'loop'
 };
 
+const STAGGER_TIME = 30;
+const SQUARE_WIDTH = 380;
+const BOARD_SCALE_PCT = 0.8;
+
+let xOffset;
+let yOffset;
+let bg;
+
 class Game {
     
-    // Set board size based on input
     constructor() {
+        // TODO: Set board size based on input
         this.BOARD_SIZE = /*document.getElementById('resize').value;*/ 6;
 
         this.directions = ['N', 'S', 'E', 'W'];
-        this.board = [];
+        this.boardArr = [];
         this.spot = {};
         this.visited = [];
+        this.squares = [];
     
-        this.createBoard(this.BOARD_SIZE);
+        this.create2dArr(this.boardArr, 'directions');
         this.state = '';
     }
-
+    
+    /*
+     *
+     *
+     *
+     */
+    create2dArr(arr, item) {
+        for (let x = 0; x < this.BOARD_SIZE; ++x) {
+            arr[x] = [];
+            for (let y = 0; y < this.BOARD_SIZE; ++y) {
+                if (item === 'directions') {
+                    // Fill board with random directions
+                    arr[x][y] = this.getRandomFromArr(this.directions);
+                } else {
+                    // Fill array with items
+                    arr[x][y] = item;
+                }
+            }
+        }
+    }
+    
     getRandomFromArr(arr) {
         return arr[Math.floor(Math.random() * arr.length)];
     }
     
-    
-    /*
-     *
-     * 
-     *
-     */
-    createBoard(size) {
-        for (let x = 0; x < this.BOARD_SIZE; ++x) {
-            this.board[x] = [];
-            for (let y = 0; y < this.BOARD_SIZE; ++y) {
-                // Fill board with random directions
-                this.board[x][y] = this.getRandomFromArr(this.directions);
+    addSquare(x, y) {
+        // TODO: Combine test.js, square.js, checker.js, reference library? How?
+        PIXI.animate.load(lib.square, board, (square) => {
+
+            xOffset = square.width / 2;
+            yOffset = square.height / 2;
+            square.x = ( x - y ) * xOffset;
+            square.y = ( y + x ) * yOffset;
+            square.hitArea = new PIXI.Polygon([-xOffset, 0, 0, yOffset, xOffset, 0, 0, -yOffset]);
+            
+            let state = square.state;
+            let frame = 0;
+            state.gotoAndStop(frame);
+            
+            square.directions.gotoAndStop(this.boardArr[x][y]);
+            
+            if (( x + y ) % 2 === 0) {
+                square.color02.visible = false;
+            } else {
+                square.color01.visible = false;
             }
-        }
+            
+            square.interactive = true;
+            
+            // TODO: This is setting all squares to the last x, y coord. FIX!
+            this.create2dArr(this.squares, square);
+
+            square.on('click', ()=>{
+                // TODO: remove click listener from other squares
+                this.dropChecker(x, y);
+            });
+            
+            PIXI.animate.Animator.play(square, 'fadeIn');
+        }, 'assets');
     }
+
+    dropChecker(x, y) {
+        PIXI.animate.load(lib.checker, board, (checker)=>{
+            this.placeChecker(checker, x, y);
+            
+            PIXI.animate.Animator.play(checker, 'dropIn', ()=>{
+                this.moveChecker(checker, x, y);
+            });
+        }, 'assets');
+    }
+
     
     /*
-     *
      * Starts player at a random square on the board
-     *
      */
     randomSpot() {
         this.visited.length = 0;
-        this.spot.x = Math.floor(Math.random() * this.BOARD_SIZE);
-        this.spot.y = Math.floor(Math.random() * this.BOARD_SIZE);
-        // console.log(this.spot.x, this.spot.y);
+        x = Math.floor(Math.random() * this.BOARD_SIZE);
+        y = Math.floor(Math.random() * this.BOARD_SIZE);
     }
     
     /*
-     *
-     * Moves one square
-     *
+     * Sets x/y position on stage
      */
-    walk(arr, x, y) {
-        // TODO: Do I need to have a this.spot or can I just use x, y?
-        console.log("Spot:", this.spot);
-        // Add prev position to array
-        this.visited.push(`${this.spot.x} ${this.spot.y}`);
-        
-        // Move based on direction
-        switch(arr[x][y]){
-          case 'N':
-            this.spot.y -= 1;
-            break;
-          case 'S':
-            this.spot.y += 1;
-            break;
-          case 'W':
-            this.spot.x -= 1;
-            break;
-          case 'E':
-            this.spot.x += 1;
-            break;
-        }
+    placeChecker(instance, x, y) {
+        instance.x = ( x - y ) * xOffset;
+        instance.y = ( y + x ) * yOffset;
+    }
+
+    moveChecker(instance, x, y) {
+        let moveAnimLabel = 'move' + this.boardArr[x][y];
+        PIXI.animate.Animator.play(instance, moveAnimLabel, ()=>{
+
+            console.log("Spot:", {x, y});
+            // Add prev position to array
+            // TODO: Fix this so it's comparing an array instead of a string
+            this.visited.push(`${x} ${y}`);
+            
+            // Move based on direction
+            switch(this.boardArr[x][y]){
+              case 'N':
+                y -= 1;
+                break;
+              case 'S':
+                y += 1;
+                break;
+              case 'W':
+                x -= 1;
+                break;
+              case 'E':
+                x += 1;
+                break;
+            }
+            this.checkPosition(instance, x, y);
+        });
+    }
+    
+    checkPosition(instance, x, y) {
         
         // If the new position is on the board, compare it to previous spots
-        if (this.spot.x >=0 && this.spot.x < this.BOARD_SIZE && this.spot.y >= 0 && this.spot.y < this.BOARD_SIZE) {
-
+        if (x >=0 && x < this.BOARD_SIZE && y >= 0 && y < this.BOARD_SIZE) {
+            
             // If spot has already been visited, we know we're in a loop
-            if (this.visited.indexOf(`${this.spot.x} ${this.spot.y}`) >= 0) {
+            if (this.visited.indexOf(`${x} ${y}`) >= 0) {
                 // TODO: Set this up to turn visited squares green
                 // TODO: Other loop animation?
                 console.log('LOOP MOTHAFUCKAAAAA');
-                this.state = states.LOOP;
-                this.done();
-            // Otherwise, we're not in a loop, keep walking
-            } else {
-                // Turn this on/off to make this recursive and
-                // keep walking until it either loops or falls off
-                console.log('next!');
-                // this.walk(arr, this.spot.y, this.spot.x);
-                return;
+                bg.gotoAndStop(states.LOOP);
             }
+            
+            // Otherwise, we're not in a loop, keep walking
+            this.placeChecker(instance, x, y);
+            this.moveChecker(instance, x, y);
+            
         // If it's not on the board, you fell off the edge!
         } else {
             // TODO: Set this up to turn visited squares red
             // TODO: Other fall-off-edge animation?
             console.log('YOU FELL OFF THE EDGEEEEE');
-            this.state = states.OFF;
-            this.done();
+            bg.gotoAndStop(states.OFF);
+            return;
         }
-    }
-    
-    // Break out of recursive loop
-    done() {
-        console.log('stopping...');
     }
 }
 
