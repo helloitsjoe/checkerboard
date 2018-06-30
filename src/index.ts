3877357227ab6549c9f089d4fec255633c17f0a9
helloitsjoe <jboyle617@gmail.com>
2018-06-29 22:52:49 -0400

    fixing index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,58 @@
 import * as PIXI from 'pixi.js';
 
 const testLib = require('../assets/test.js');
+
+const Game = require('./game');
+const game = new Game();
+console.log('PIXI', PIXI.animate);
+
+const app = new PIXI.Application(1280, 720, {backgroundColor : 0x0});
+const container = document.getElementById('container');
+container.appendChild(app.view);
+app.stage.addChild(game.stage);
+
+PIXI.animate.load(testLib.stage, game.stage, (instance) => {
+    game.board.setTheTable(instance);
+}, 'assets');
+
 // import animate from 'pixi-animate';
 // require('../assets/checker.js');
 // require('../assets/square.js');
 
-import Game from './Game';
-import Board from './Board';
+// import Game from './Game';
+// import Board from './Board';
 // import Gui from './Gui';
 // import Checker from './Checker';
 // var config = require('./gameConfig.json');
 
-const renderer = new PIXI.autoDetectRenderer(1280, 720, {
-    view: document.getElementById("stage"),
-    backgroundColor: 0x0,
-    antialias: true
-});
-const stage = new PIXI.Container();
+// const renderer = new PIXI.autoDetectRenderer(1280, 720, {
+//     view: document.getElementById("stage"),
+//     backgroundColor: 0x0,
+//     antialias: true
+// });
+// const stage = new PIXI.Container();
 
-const game = new Game();
-const board = new Board(game);
+// const game = new Game();
+// const board = new Board(game);
 // const gui = new Gui(game);
 // const checker = new Checker(game);
 
-PIXI.animate.load(testLib, stage, (test) => {
-    board.setTheTable(test)
-}, 'assets');
-update();
+// PIXI.animate.load(testLib, stage, (test) => {
+//     board.setTheTable(test)
+// }, 'assets');
+// update();
 
-function update() {
-    renderer.render(stage);
-    requestAnimationFrame(update);
-}
+// function update() {
+//     renderer.render(stage);
+//     requestAnimationFrame(update);
+// }
 
-function playAudio(sound, ms) {
-    setTimeout(()=>{
-        new Audio(`./audio/${sound}.wav`).play();
-    }, ms)
-}
+// function playAudio(sound, ms) {
+//     setTimeout(()=>{
+//         new Audio(`./audio/${sound}.wav`).play();
+//     }, ms)
+// }
 
+// require('pixi.js');
+// require('pixi-animate');
+// const test = require('.../assets/test');
