1bd975fa0a48ed11df48f8168abc12357d06bea3
helloitsjoe <jboyle617@gmail.com>
2018-07-01 00:26:49 -0400

    temp remove tests

--- test/game.test.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const assert = require('assert');
-const Game = require('../src/game');
-
-describe('Game', () => {
-    it('does a thing', function () {
-        const game = new Game();
-    });
-});
